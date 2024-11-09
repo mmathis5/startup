5d2f2d9655eb61c2c4e8379a21100e08d30c42bf
Madeline Mathis <madelinemathis79@gmail.com>
2024-11-09 13:01:47 -0700

    working on login stuff

---
+++ startupVite/src/login/authenticated.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import './authenticated.css';
+
+export function Authenticated(props) {
+    const navigate = useNavigate();
+    
+    function logout(){
+        localStorage.remove('userName');
+        props.onLogout();
+    }
+
+    return(
+        <div>
+            <div className = 'playerName'>{props.userName}</div>
+            <Button variant='primary' onclick={() => navigate('/connect')}>
+                Connect
+            </Button>
+            <Button variant='secondary' onclick={() => logout()}>
+                Logout
+            </Button>
+        </div>
+    );
+}

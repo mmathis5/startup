e4944a998f3f9b20b74f3104ca6256ef0580e386
Madeline Mathis <madelinemathis79@gmail.com>
2024-11-09 14:12:17 -0700

    added connect capabilities

---
+++ startupVite/src/connect/connect.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import './connect.css'
+
+export function Connect({userName}){
+    const[partnerEmail, setPartnerEmail] = useState('');
+
+    const handleEmailChange = (event) => {
+        setPartnerEmail('event.target.value');
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log('User ${userName} connected with partner ${partnerEmail}')
+    };
+
+    return(
+
+      <main>
+      <h1>Connect With A Partner</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="rounded-box">
+          <label>Partner Email:</label>
+          <input 
+            type="email" 
+            placeholder="partneremail@domain.com" 
+            value={partnerEmail}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
+    </main>
+    );
+}

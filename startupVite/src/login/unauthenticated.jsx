8ec6fb12f8f8288c279cf41d59bc6f9ad0e4ba6d
Madeline Mathis <madelinemathis79@gmail.com>
2024-11-17 17:23:11 -0700

    added login and create user service endpoint

--- startupVite/src/login/unauthenticated.jsx
+++ startupVite/src/login/unauthenticated.jsx
@@ -6,14 +6,29 @@ export function Unauthenticated(props){
     const [userName, setUserName] = React.useState(props.username);
     const [password, setPassword] = React.useState('');
     
-    async function loginUser(){
-        localStorage.setItem('userName', userName);
-        props.onLogin(userName);
+    async function loginUser() {
+      loginOrCreate(`/api/auth/login`);
     }
-
-    async function createUser(){
+  
+    async function createUser() {
+      loginOrCreate(`/api/auth/create`);
+    }
+  
+    async function loginOrCreate(endpoint) {
+      const response = await fetch(endpoint, {
+        method: 'post',
+        body: JSON.stringify({ email: userName, password: password }),
+        headers: {
+          'Content-type': 'application/json; charset=UTF-8',
+        },
+      });
+      if (response?.status === 200) {
         localStorage.setItem('userName', userName);
         props.onLogin(userName);
+      } else {
+        const body = await response.json();
+        setDisplayError(`⚠ Error: ${body.msg}`);
+      }
     }
 
     return (
